9686a3a81ddafb5043eeaa19441290e4d0f6954d
skrowll <lucasdejesus.rocha@hotmail.com>
2025-07-10 21:06:50 -0300

    chore: update dependencies and add bcryptjs support
    - Added bcryptjs version 3.0.2 to dependencies for password hashing.
    - Updated @types/node to version 20.19.7 for improved type definitions.
    - Added @types/bcryptjs version 2.4.6 for TypeScript support with bcryptjs.
    - Reintroduced prisma version 6.11.1 to dependencies.
    - Updated typescript to version 5.8.3 and added ts-node version 10.9.2 for better TypeScript development experience.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/.next/**",
+      "src/generated/**/*",
+      "!src/generated/**/*.ts"
+    ],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"]
+  }
 ];
 
 export default eslintConfig;
