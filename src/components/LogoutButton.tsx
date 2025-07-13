68d21e1730083795ca1d168b044f5aece64eae99
skrowll <lucasdejesus.rocha@hotmail.com>
2025-07-13 17:32:35 -0300

    feat: enhance LogoutButton with icon and improved styling

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,14 +1,18 @@
 "use client";
 
+import { LogOut } from "lucide-react";
 import { signOut } from "next-auth/react";
 
 export default function LogoutButton() {
   return (
     <button
       onClick={() => signOut({ callbackUrl: "/auth/login" })}
-      className="px-4 py-2 bg-red-600 text-white text-sm font-medium rounded-md hover:bg-red-700 transition-colors cursor-pointer"
+      className="px-4 py-2  bg-red-800 dark:bg-red-500 text-white text-sm font-medium rounded-md hover:bg-red-900 dark:hover:bg-red-700 transition-colors cursor-pointer"
     >
-      Sair
+      <span className="flex items-center gap-2">
+        <LogOut className="w-5 h-5" />
+        Logout
+      </span>
     </button>
   );
 }
